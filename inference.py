5d7d4bfcff6c242097d293c9af2c993614cb2cdd
NASEEM A P <naseemalassampattil@gmail.com>
2023-06-15 16:12:48 +0530

    added option to image and hide option

--- inference.py
+++ inference.py
@@ -1,4 +1,3 @@
-from keras.models import load_model
 from ultralytics import YOLO
 from utils import norm_kpts, plot_one_box, plot_skeleton_kpts, load_model_ext
 import pandas as pd
@@ -7,6 +6,7 @@ import time
 import os
 import argparse
 import json
+import random
 
 
 ap = argparse.ArgumentParser()
@@ -18,6 +18,8 @@ ap.add_argument("-s", "--source", type=str, required=True,
                 help="path to video/cam/RTSP")
 ap.add_argument("--save", action='store_true',
                 help="Save video")
+ap.add_argument("--hide", action='store_false',
+                help="to hide inference window")
 args = vars(ap.parse_args())
 
 col_names = [
@@ -29,40 +31,7 @@ col_names = [
 # YOLOv8 Pose Model
 model = YOLO('yolov8n-pose.pt')
 
-# Keras pose model
-saved_model, meta_str = load_model_ext(args['model'])
-class_names = json.loads(meta_str)
-
-# Load video/cam/RTSP
-if args['source'].isnumeric():
-    cap = cv2.VideoCapture(int(args['source']))
-else:
-    cap = cv2.VideoCapture(args['source'])
-
-# Write Video
-if args['save']:
-    # Get the width and height of the video.
-    original_video_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    original_video_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    
-    # path to save videos
-    os.makedirs(os.path.join('runs', 'detect'), exist_ok=True)
-    if not args['save'].isnumeric():
-        video_name = os.path.split(args['save'])[1]
-    else:
-        video_name = f"cam{args['save']}"
-    out_vid = cv2.VideoWriter(os.path.join('runs', 'detect', video_name), 
-                         cv2.VideoWriter_fourcc(*'mp4v'),
-                         fps, (original_video_width, original_video_height))
-
-p_time = 0
-while True:
-    success, img = cap.read()
-    if not success:
-        print('[INFO] Failed to Read...')
-        break
-
+def get_inference(img):
     results = model.predict(img)
     for result in results:
         for box, pose in zip(result.boxes, result.keypoints):
@@ -84,24 +53,101 @@ while True:
                     pose_class = 'Unknown Pose'
                     print('[INFO] Predictions is below given Confidence!!')
 
-                plot_one_box(box.xyxy[0], img, (255, 0, 255), f'{pose_class} {max(predict)}')
+                plot_one_box(box.xyxy[0], img, colors[predict.argmax()], f'{pose_class} {max(predict)}')
                 plot_skeleton_kpts(img, pose, radius=5, line_thick=2, confi=0.5)
 
-    # Write Video
-    if args['save']:
-        out_vid.write(img)
+
+# Keras pose model
+saved_model, meta_str = load_model_ext(args['model'])
+class_names = json.loads(meta_str)
+colors = [[random.randint(0, 255) for _ in range(3)] for _ in class_names]
+
+# Inference Image
+if args['source'].endswith('.jpg') or args['source'].endswith('.jpeg') or args['source'].endswith('.png'):
+    img = cv2.imread(args['source'])
+    get_inference(img)
+
+    # save Image
+    if args['save'] or args['hide'] is False:
+        os.makedirs(os.path.join('runs', 'detect'), exist_ok=True)
+        path_save = os.path.join('runs', 'detect', os.path.split(args['source'])[1])
+        cv2.imwrite(path_save, img)
+        print(f"[INFO] Saved Image: {path_save}")
     
-    # FPS
-    c_time = time.time()
-    fps = 1/(c_time-p_time)
-    print('FPS: ', fps)
-    p_time = c_time
+    # Hide video
+    if args['hide']:
+        cv2.imshow('img', img)
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
 
-    cv2.imshow('Output', img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# Inference on Video/Cam/RTSP
+else:
+    # Load video/cam/RTSP
+    video_path = args['source']
+    if video_path.isnumeric():
+        video_path = int(video_path)
+    cap = cv2.VideoCapture(video_path)
 
-cap.release()
-if args['save']:
-    out_vid.release()
-cv2.destroyAllWindows()
+    # Total Frame count
+    if args['hide'] is False:
+        length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_count = 0
+
+    # Write Video
+    if args['save'] or args['hide'] is False:
+        # Get the width and height of the video.
+        original_video_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        original_video_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        
+        # path to save videos
+        os.makedirs(os.path.join('runs', 'detect'), exist_ok=True)
+        if not str(video_path).isnumeric():
+            path_save = os.path.join('runs', 'detect', os.path.split(video_path)[1])
+        else:
+            c = 0
+            while True:
+                if not os.path.exists(os.path.join('runs', 'detect', f'cam{c}.mp4')):
+                    path_save = os.path.join('runs', 'detect', f'cam{c}.mp4')
+                    break
+                else:
+                    c += 1
+        out_vid = cv2.VideoWriter(path_save, 
+                            cv2.VideoWriter_fourcc(*'mp4v'),
+                            fps, (original_video_width, original_video_height))
+
+    p_time = 0
+    while True:
+        success, img = cap.read()
+        if not success:
+            print('[INFO] Failed to Read...')
+            break
+
+        get_inference(img)
+        if args['hide'] is False:
+            frame_count += 1
+            print(f'Frames Completed: {frame_count}/{length}')
+
+        # FPS
+        c_time = time.time()
+        fps = 1/(c_time-p_time)
+        print('FPS: ', fps)
+        p_time = c_time
+        
+        # Write Video
+        if args['save'] or args['hide'] is False:
+            out_vid.write(img)
+
+        # Hide video
+        if args['hide']:
+            cv2.imshow('img', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+
+    cap.release()
+    if args['save'] or args['hide'] is False:
+        out_vid.release()
+        print(f"[INFO] Outout Video Saved in {path_save}")
+    if args['hide']:
+        cv2.destroyAllWindows()
